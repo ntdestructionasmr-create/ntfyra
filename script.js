f29d14326247391f1493e42fbe81f897103bf433
ntdestructionasmr-create <ntdestructionasmr@gmail.com>
2025-12-26 00:37:56 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Save user data from onboarding
-function saveUserData() {
-  const weight = document.getElementById("weight").value;
-  const goal = document.getElementById("goal").value;
-
-  localStorage.setItem("weight", weight);
-  localStorage.setItem("goal", goal);
-}
